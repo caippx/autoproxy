46802b1a469f47a4f2f615a0a0e213b9d31c0c94
caippx <37325838+caippx@users.noreply.github.com>
2020-02-22 18:33:01 +0800

    Update ssrjs.sh

--- ssrjs.sh
+++ ssrjs.sh
@@ -1191,8 +1191,8 @@ Install_Auto(){
 	apt-get update -y
 	apt-get install -y curl
 	echo && stty erase '^H' && read -p "输入域名(提前绑定到VPS IP): " domain
-	[[ -z ${domain} ]] && echo "域名为空，使用自带免费域名" && curl https://raw.githubusercontent.com/EtherDream/jsproxy/0.1.0/i.sh | bash
-	[[ -n ${domain} ]] && curl https://raw.githubusercontent.com/EtherDream/jsproxy/0.1.0/i.sh | bash -s $domain
+	[[ -z ${domain} ]] && echo "域名为空，使用自带免费域名" && curl https://raw.githubusercontent.com/caippx/jsproxy/0.1.0/i.sh | bash
+	[[ -n ${domain} ]] && curl https://raw.githubusercontent.com/caippx/jsproxy/0.1.0/i.sh | bash -s $domain
 	Install_SSR
 }
 check_sys
